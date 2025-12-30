084144747d35b90e86b71f925fda043ddddaacfa
Ef-LoveRikuu <zsyzsy2005@outlook.com>
2025-12-30 10:05:54 +0800

    完善了页面并加入了加入的节点查询以及节点启动时间功能

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ![EthExplorer Screenshot](http://i.imgur.com/NHFYq0x.png)
 
-##License
+## License
 
 GPL (see LICENSE)
 
-##Installation
+## Installation
 
 Install [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git "Git installation") if you haven't already
 
@@ -27,3 +27,13 @@ Then visit http://localhost:8000 in your browser of choice. You might get an err
 Install [geth](https://github.com/ethereum/go-ethereum/wiki/Building-Ethereum "Geth install") if you don't already have it, then run the above command.
 
 Then refresh the page in your browser 
+
+# 我的修改
+1.修改了网页的名称为：zsy的Ether Block Explorer  
+2.修改页脚版权信息 (Footer) 为 2025 张舒扬实训项目 | 基于 Etherparty 开源改造
+3.将输入栏的内容改为中文方便阅读并且加长了长度，不至于显示不完
+4.     在app.js中增加控制台调试日志，方便监控连接状态
+        console.log("=== 张舒扬实训项目启动 ===");
+        console.log("正在尝试连接本地以太坊节点: " + eth_node_url);
+5.在app.js中加入了查询geth是否开启成功的功能，并在网页端那里写上了启动时间
+6.在app.js中加入了查询连接的peer数量并且在main.html下面加入了可以查询的命，查询成功了
--- app/app.js
+++ app/app.js
@@ -28,8 +28,34 @@ angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
     .run(function($rootScope) {
         var web3 = new Web3();
         var eth_node_url = 'http://localhost:8545'; // TODO: remote URL
+        console.log("=== 张舒扬实训项目启动 ===");
+        console.log("正在尝试连接本地以太坊节点: " + eth_node_url);
 	web3.setProvider(new web3.providers.HttpProvider(eth_node_url));
         $rootScope.web3 = web3;
+        if(web3.isConnected()) {
+            // 获取连接到的节点数量（即你的 Team 成员数）
+            $rootScope.peerCount = web3.net.peerCount;
+            
+            // 如果你想显示具体是谁，可以尝试获取节点 ID（需要 admin 权限）
+            try {
+                $rootScope.nodeInfo = web3.admin.nodeInfo;
+                console.log("我的节点 ID: " + $rootScope.nodeInfo.id);
+            } catch(e) {
+                console.log("无法获取详细 Peer 信息，请确保 Geth 开启了 admin API");
+            }
+        }
+        
+        $rootScope.startTime = new Date().toLocaleString();
+
+        if(!web3.isConnected()) {
+            console.error("[错误] 无法连接到节点。请检查：1.Geth是否启动 2.是否开启了 --http.corsdomain '*' ");
+            $('#connectwarning').modal({keyboard:false,backdrop:'static'}) 
+            $('#connectwarning').modal('show') 
+        } else {
+            console.log("[成功] 已成功连接至以太坊区块链节点。");
+            console.log("系统启动时间: " + $rootScope.startTime);
+        }
+
         function sleepFor( sleepDuration ){
             var now = new Date().getTime();
             while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
